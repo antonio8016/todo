c0ceb85c8a9ffaf428c4c24714ccd138df4478ab
Antonio Rodriguez <antonio.rodriguez@sas.com>
2014-02-01 15:00:41 -0500

    First commit. Adding a README.

---
+++ routes/item.js
@@ -0,0 +1,68 @@
+
+/*
+ * GET items.
+ */
+exports.index = function(req, res){
+
+	var item1 = { 
+		name: "Milk" 
+	};
+
+	var item2 = {
+		name: "Bread"
+	};
+
+	var items = [item1, item2];
+
+  	res.json(items);
+
+};
+
+/*
+ *	create item
+ */
+exports.create = function(req, res) {
+
+	var name = req.body.name;
+
+	var item = {
+		name : name
+	};
+
+	res.json(item);
+
+};
+
+/*
+ *	edit item
+ */
+exports.update = function(req, res) {
+
+	var name = req.body.name;
+	var id = req.params.id;
+
+	var item = {
+		name : name,
+		id: id
+	};
+
+	res.json(item);
+
+};
+
+/*
+ *	Delete item
+ */
+exports.destroy = function(req, res) {
+
+	var id = req.params.id;
+
+	var item = {
+		id: id
+	};
+
+	res.json(item);
+
+}
+
+
