1176a4d16f42d4091cfdccbce5a092ed82441e09
Antonio Rodriguez <antonio.rodriguez@sas.com>
2014-02-07 08:35:57 -0500

    Renaming ‘routes’ to ‘controllers’.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var item = require('./routes/item');
+var controllers = require('./controllers');
+var user = require('./controllers/user');
+var item = require('./controllers/item');
 var http = require('http');
 var path = require('path');
 var db = require('./models');
@@ -31,8 +31,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Refactor structure
+// http://stackoverflow.com/questions/5178334/folder-structure-for-a-nodejs-project
+
+// Refactor controllers
+// http://benedmunds.com/2012/04/19/simple-nodejs-express-mvr-template/
 // Root route
-app.get('/', routes.index);
+app.get('/', controllers.index);
 
 // Users
 app.get('/users', user.list);
@@ -57,13 +62,35 @@ db
         	console.log('Express server listening on port ' + app.get('port'))
       	})
 
+      	// Refactor sockets
+      	// http://stackoverflow.com/questions/12500922/nodejs-include-socket-io-in-router-page
+      	// http://stackoverflow.com/questions/18856190/use-socket-io-inside-a-express-routes-file
       	io = require('socket.io').listen(server)
 
       	io.sockets.on('connection', function (socket) {
   			socket.emit('news', { hello: 'world' });
-  			socket.on('my_other_event', function (data) {
-    			console.log(data);
+
+  			// Item resource
+  			socket.on('list_items', function (data) {
+    			db.Item.all().success(function(users) {
+    			   socket.emit('list_items', JSON.stringify(users));
+   	 			});
   			});
+
+  			socket.on('create_item', function(data) {
+  				db.Item.create({ name : data.body.name }).success(function(item) {
+              socket.emit('create_item', JSON.stringify(item));
+          })
+  			});
+  			
+        socket.on('edit_item', function(data) {
+  				console.log('edit_item');
+  			});
+      	
+        socket.on('destroy_item', function(data) {
+      			console.log('destroy_item');
+      	});
+
 		});
 
 		io.sockets.on('disconnect', function (socket) {
