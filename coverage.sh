faede4cf21ccab3c5b3b6b01461e012f4698a942
Antonio Rodriguez <antonio.rodriguez@sas.com>
2014-02-12 21:24:20 -0500

    Adding more steps to the coverage script.

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Start the db
+# mongod -dbpath ~/data/db
+
+# Run mocha using istanbul
 istanbul cover _mocha -- -R spec
+
+# Open the coverage report
 open coverage/lcov-report/index.html
