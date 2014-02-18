e3c995e814cfda4a21cde701e3abe90f254d474e
Antonio Rodriguez <antonio.rodriguez@sas.com>
2014-02-18 13:06:40 -0500

    Enabling grunt support.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'lib/*.js', 'controllers/*.js', 'config/*.js', 'constraints/*.js', 'test/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('test', ['jshint']);
+
+  grunt.registerTask('default', ['jshint']);
+
+};
