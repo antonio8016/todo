d6b348b6600e00ebe6ca18cf5a08dd62fc04283c
Antonio Rodriguez <antonio.rodriguez@sas.com>
2014-02-18 15:06:21 -0500

    Adding stubs for custom grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,29 @@
+var generate_controller = require('./tasks/generate_controller'),
+    generate_model = require('./tasks/generate_model');
+
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      files: ['Gruntfile.js', 'lib/*.js', 'controllers/*.js', 'config/*.js', 'constraints/*.js', 'test/*.js', 'test/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['Gruntfile.js', 'lib/*.js', 'controllers/*.js', 'config/*.js', 'constraints/*.js', 'tasks/*.js', 'test/*.js', 'test/**/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('default', ['jshint']);
 
-  grunt.registerTask('default', ['jshint']);
-
+    grunt.registerTask('gen-controller', '<<NOTHING>>', generate_controller.task(grunt));
+    grunt.registerTask('gen-model', '<<NOTHING>>', generate_model.task(grunt));
 };
---
+++ tasks/generate_controller.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+exports.task = function(grunt) {
+    return function() {
+        grunt.log.writeln('Generating a controller...');
+        var done = this.async();
+        fs.writeFile("./banana.js", "Hey there!", function(err) {
+            if(err) {
+                grunt.log.writeln(err);
+            } else {
+                grunt.log.writeln("The file was saved!");
+            }
+            done();
+        }); 
+    };
+};
